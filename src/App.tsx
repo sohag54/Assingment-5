69b515f1a65044b7a66d7e258b7bb41d6ebbf6c0
sohag54 <sohag36057@gmail.com>
2026-09-25 21:42:43 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default App
